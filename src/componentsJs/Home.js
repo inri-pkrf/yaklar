b3de262fe40d2d8db3a16c6bb3e405af2812618e
Alma-yovell <alma.yovell@gmail.com>
2025-02-06 14:36:41 +0200

    startng home page

--- src/componentsJs/Home.js
+++ src/componentsJs/Home.js
@@ -11,9 +11,31 @@ function Home() {
 
             <div className="title">
                 לומדת
-                <br/>
+                <br />
                 מש"ק התנדבות ביקל"ר
+            </div >
+
+            <div className="home-text">
+                יש ללחוץ ולעבור על התוכן בכל קופסא בכדי להעשיר את ארגז הכלים שלכם לפעולה. יש להתקדם לפי הסדר כדי לחשוף את התכנים.
+                <br /><br />
+                בסיום כל נושא נשאל כמה שאלות הבנה שחובה לענות עליהן כדי להתקדם. זה יעזור לנו ולכם להבין שכולנו בכיוון הנכון.
             </div>
+
+            <div className="flexBox">
+
+                {[...Array(4)].map((_, index) => (
+                    <img
+                        key={index}
+                        src={process.env.PUBLIC_URL + '/assests/imgs/boxClose.png'}
+                        className="boxClose"
+                        alt="boxClose"
+                    />
+                ))}
+            </div>
+
+            {/* <div className="flexBox">
+                <img src={process.env.PUBLIC_URL + '/assests/imgs/boxClose.png'} className="boxClose" alt="boxClose" />
+            </div> */}
 
 
         </div>
